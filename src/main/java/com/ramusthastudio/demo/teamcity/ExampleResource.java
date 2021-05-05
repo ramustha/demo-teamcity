e45483b21fcca07b57acd39ce2a85c75a3ac8f87
Ramadhani Musthofa <ramusthanest@gmail.com>
2021-05-05 14:27:48 +0700

    update endpoint

--- src/main/java/com/ramusthastudio/demo/teamcity/ExampleResource.java
+++ src/main/java/com/ramusthastudio/demo/teamcity/ExampleResource.java
@@ -1,24 +1,23 @@
 package com.ramusthastudio.demo.teamcity;
 
-import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.Multi;
+import org.jboss.resteasy.reactive.RestSseElementType;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.security.SecureRandom;
 import java.time.Duration;
+import java.time.LocalDateTime;
 
-@Path("/hello")
+@Path("/stream")
 public class ExampleResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public Uni<Integer> hello() {
-        return Uni.createFrom()
-                .item(() -> new SecureRandom().nextInt(1000))
-                .onItem()
-                .delayIt()
-                .by(Duration.ofSeconds(2));
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    @RestSseElementType(MediaType.APPLICATION_JSON)
+    public Multi<String> hello() {
+        return Multi.createFrom().ticks().every(Duration.ofSeconds(1))
+                .onItem().transform(n -> String.format("Date %s", LocalDateTime.now()));
     }
 }
